2c80390b391ae7918ba7c7c49963e7dcf8fe0baf
GenYans <genapiter@yandex.ru>
2023-03-09 15:59:52 +0300

    Lesson №84

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
     
 /* Timer */
 
-const deadLine = '2023-02-22T16:16:00';
+const deadLine = '2023-03-20T16:16:00';
 
     function getTimeRemaining(endtime) {
         let days,hours,minutes,seconds;
@@ -216,6 +216,58 @@ const deadLine = '2023-02-22T16:16:00';
         7,
         '.menu .container' /* Родительский селектор */
     ).render();
+
+    /* Forms */
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо, скоро с вами свяжется наш оператор',
+        failure: 'Упс, что-то пошло не так'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit',(e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if(request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });  
+        });
+    }
 });
 
 
