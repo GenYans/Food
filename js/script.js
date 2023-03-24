84b238394dd25c7b32502e6820925244498f3875
GenYans <genapiter@yandex.ru>
2023-03-24 15:54:29 +0300

    Lesson №91

--- js/script.js
+++ js/script.js
@@ -37,8 +37,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-
-    
 /* Timer */
 
 const deadLine = '2023-03-20T16:16:00';
@@ -187,32 +185,53 @@ const deadLine = '2023-03-20T16:16:00';
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
-        9,
-        '.menu .container', /* Родительский селектор */
-    ).render();
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню "Премиум"',
-        "Меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-        15,
-        '.menu .container' /* Родительский селектор */
-    ).render();
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-        7,
-        '.menu .container' /* Родительский селектор */
-    ).render();
+        }
+
+        return await res.json();
+    };
+
+/*     getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr,price}) => {
+                new MenuCard(img, altimg, title, descr,price, '.menu .container').render();
+            });
+        }); */
+
+        getResource('http://localhost:3000/menu')
+        .then(data => createCard(data));
+
+        function createCard(data) {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                const element = document.createElement('div');
+                
+                element.classList.add('menu__item');
+
+                element.innerHTML = `
+                <img src=${img} alt=${altimg}>
+                <h3 class="menu__item-subtitle">${title}</h3>
+                <div class="menu__item-descr">${descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${price}</span> руб/день</div>
+            </div>`;
+
+            document.querySelector('.menu .container').append(element);
+            });
+        }
+
+/*     axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr,price}) => {
+                new MenuCard(img, altimg, title, descr,price, '.menu .container').render(); 
+            });
+        }); */
 
     /* Forms */
 
@@ -225,10 +244,22 @@ const deadLine = '2023-03-20T16:16:00';
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindpostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
+
+    function bindpostData(form) {
         form.addEventListener('submit',(e) => {
             e.preventDefault();
 
@@ -244,19 +275,9 @@ const deadLine = '2023-03-20T16:16:00';
             /* request.setRequestHeader('Content-type', 'application/json'); */
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
@@ -304,7 +325,8 @@ const deadLine = '2023-03-20T16:16:00';
             closeModal();
         }, 4000);
     }
+
+fetch('http://localhost:3000/menu')
+    .then(data => data.json)
+    .then(res => console.log(res));
 });
-
-
-
